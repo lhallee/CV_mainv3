267eda0b0b6842287591700285c05a7e7a89e139
Logan Hallee <lhallee99@gmail.com>
2023-02-20 16:51:26 -0500

    new reconstruction visualization

--- Alignment.py
+++ Alignment.py
@@ -1,64 +1,73 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from natsort import natsorted
+from glob import glob
 from scipy.ndimage import rotate
 
 
 def align_images(im1, im2):
     # Convert images to grayscale
-    scale_percent = 10  # percent of original size
-    width = int(im1.shape[1] * scale_percent / 100)
-    height = int(im1.shape[0] * scale_percent / 100)
-    dim = (width, height)
-    im1_scale = cv2.resize(im1, dim)
-    im2_scale = cv2.resize(im2, dim)
-    print(im2_scale.shape)
     # Find size of image1
-    sz = im1_scale.shape
-
+    sz = im1.shape
     # Define the motion model
     warp_mode = cv2.MOTION_EUCLIDEAN
-
     # Define the warp matrix
     warp_matrix = np.eye(2, 3, dtype=np.float32)
-
     # Define the number of iterations
-    number_of_iterations = 20000
+    number_of_iterations = 500
     # Define correlation coefficient threshold
     # Specify the threshold of the increment in the correlation coefficient between two iterations
     termination_eps = 1e-10
-
     # Define termination criteria
     criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, number_of_iterations, termination_eps)
-
     # Run the ECC algorithm. The results are stored in warp_matrix.
-    (cc, warp_matrix) = cv2.findTransformECC(im1_scale, im2_scale, warp_matrix, warp_mode, criteria)
-
+    (cc, warp_matrix) = cv2.findTransformECC(im1, im2, warp_matrix, warp_mode, criteria)
     # Use warpPerspective for Homography
-    im_aligned = cv2.warpAffine(im2_scale, warp_matrix, (sz[1], sz[0]),
+    im_aligned = cv2.warpAffine(im2, warp_matrix, (sz[1], sz[0]),
                                 flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
-
     return im_aligned, warp_matrix
 
+'''
 top, bottom, left, right = 500, 500, 500, 500
 
 
-temp = cv2.copyMakeBorder(np.array(cv2.imread('img.png', 2))[:, 3584:],
+temp = cv2.copyMakeBorder(np.array(cv2.imread('img.png', 2), dtype=np.float32),
                           top, bottom, left, right, borderType=cv2.BORDER_CONSTANT, value=0)
 
-img = rotate(cv2.copyMakeBorder(np.array(cv2.imread('img.png', 2))[:, 3584:],
+img = rotate(cv2.copyMakeBorder(np.array(cv2.imread('img.png', 2), dtype=np.float32),
                           top, bottom, left, right, borderType=cv2.BORDER_CONSTANT, value=0),
-             60, reshape=False)
+             30, reshape=False)
 
-align, warp_matrix = align_images(temp, img)
+scale_percent = 10  # percent of original size
+width = int(temp.shape[1] * scale_percent / 100)
+height = int(temp.shape[0] * scale_percent / 100)
+dim = (width, height)
+im1 = cv2.resize(temp, dim)
+im2 = cv2.resize(img, dim)
 
+align, warp_matrix = align_images(im1, im2)
+'''
 
-plt.imshow(temp)
-plt.show()
-plt.imshow(img)
-plt.show()
-plt.imshow(align)
-plt.show()
-plt.imshow(cv2.warpAffine(img, warp_matrix, (img.shape[1], img.shape[0]),
-                                flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP))
-plt.show()
+stacks_path = 'C:/Users/Logan Hallee/Desktop/1_26_result/result/'
+lop = natsorted(glob(stacks_path + 'sec*'))
+print(lop)
+full_paths = []
+for i in range(len(lop)):
+    full_paths.append(natsorted(glob(lop[i] + '/*.png')))
+
+for i in range(len(full_paths)-1):
+    template = np.array(cv2.imread(full_paths[i][-1], 2)[:, 5632:], dtype=np.float32)
+    to_rot = np.array(cv2.imread(full_paths[i+1][0], 2)[:, 5632:], dtype=np.float32)
+    big_sz = template.shape
+    scale_percent = 10  # percent of original size
+    width = int(big_sz[1] * scale_percent / 100)
+    height = int(big_sz[0] * scale_percent / 100)
+    dim = (width, height)
+    im1 = cv2.resize(template, dim)
+    sz = im1.shape
+    im2 = cv2.resize(to_rot, dim)
+    aligned, warp_matrix = align_images(im1, im2)
+    for i in range(len(full_paths[i+1])):
+        im_aligned = cv2.warpAffine(im2, warp_matrix, (sz[1], sz[0]),
+                                flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
--- interp.py
+++ interp.py
@@ -2,28 +2,26 @@ import numpy as np
 import cv2
 import scipy
 import matplotlib.pyplot as plt
+import open3d
 from natsort import natsorted
 from glob import glob
 from tqdm import tqdm
-
 
 file_path = 'C:/Users/Logan Hallee/Desktop/1_26_result/result/'
 save_path = './result/'
 img_paths = natsorted(glob(file_path + '*.png'))
 
-d = 16
-r = 0.01
+r = 0.2
 h, w = np.array(cv2.imread(img_paths[0], 2)).shape
-h, w = int(h * r), int(w/2 * r)
-preds = np.concatenate([np.array(cv2.resize(cv2.imread(img_paths[i], 2), (h, w))).T.reshape(h, w, 1)
+h, w = int(h * r), int(w / 2 * r)
+preds = np.concatenate([np.array(cv2.resize(cv2.imread(img_paths[i], 2)[:, :5632], (h, w))).reshape(h, w, 1)
                         for i in tqdm(range(len(img_paths)))], axis=2)
-
-#plt.imshow(preds[:,:,9])
-#plt.show()
+# plt.imshow(preds[:,:,0])
+# plt.show()
 
 X, Y, Z = preds.shape
 
-s = 16
+s = 16 * r
 grid_x = np.arange(0, X, 1)
 grid_y = np.arange(0, Y, 1)
 grid_z = np.arange(0, Z, 1) * s
@@ -32,14 +30,45 @@ grid = (grid_x, grid_y, grid_z)
 print('Sparse grid done')
 interpolation = scipy.interpolate.RegularGridInterpolator(grid, preds)
 print('Sparse Interpolation done')
-c = 0.5
-sZ = (Z-1) * s
-interp_arr = np.mgrid[0:X-1:c, 0:Y-1:c, 0:sZ:c].reshape(3, int(((X-1)/c)*((Y-1)/c)*(sZ/c))).T
+c = 1
+sZ = (Z - 1) * s
+sc = int(Z / c)
+# assert sZ % c == 0, 'Voxel scales and z stack scale must be divisible for mgrid to work'
+print(X, Y, sZ, sc)
+interp_arr = np.mgrid[0:X - 1:c, 0:Y - 1:c, 0:sZ:(sZ / sc)].reshape(3, int(((X - 1) / c) * ((Y - 1) / c) * sc)).T
 interp_grid = interp_arr.tolist()
 print('Dense grid done')
-V = interpolation(interp_grid)
+V = np.array(interpolation(interp_grid))
 print('Dense Interpolation done')
-V3D = V.reshape(int((X-1)/c), int((Y-1)/c), int(sZ/c))
+points = np.array(interp_grid)
+print(points.shape)
+zero_colors = np.zeros((len(V), 2))
+colors = np.hstack((V.reshape(len(V), 1), zero_colors)) / 255
+
+deletes = []
+for i in tqdm(range(len(colors)), desc='Removing Background'):
+	if colors[i][0] < 0.5:
+		deletes.append(i)
+
+points = np.delete(points, deletes, axis=0)
+colors = np.delete(colors, deletes, axis=0)
+# V3D = V.reshape(int((X-1)/c), int((Y-1)/c), int(sZ/c))
+
+pcd = open3d.geometry.PointCloud()
+pcd.points = open3d.utility.Vector3dVector(points)
+pcd.colors = open3d.utility.Vector3dVector(colors)
+
+
+# mesh = open3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(pcd, 0.03)
+# mesh.compute_vertex_normals()
+# open3d.visualization.draw_geometries([mesh], mesh_show_back_face=True)
+open3d.visualization.draw_geometries([pcd])
+def rotate_view(vis):
+	ctr = vis.get_view_control()
+	ctr.rotate(10.0, 0.0)
+	return False
+
+#open3d.visualization.draw_geometries_with_animation_callback([pcd], rotate_view)
 
 
 def plot4d(data):
@@ -57,7 +86,6 @@ def plot4d(data):
 	plt.tight_layout()
 	plt.savefig("test_scatter_4d.png", dpi=250)
 	print('Saved')
-	#plt.show()
+# plt.show()
 
-plot4d(V3D)
-
+# plot4d(V3D)
