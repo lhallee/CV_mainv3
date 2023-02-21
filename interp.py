529815b92b5a5426c396a809a08525cb208aeb62
Logan Hallee <lhallee99@gmail.com>
2023-02-21 12:08:19 -0500

    vis to class

--- interp.py
+++ interp.py
@@ -3,89 +3,106 @@ import cv2
 import scipy
 import matplotlib.pyplot as plt
 import open3d
+import argparse
 from natsort import natsorted
 from glob import glob
 from tqdm import tqdm
 
-file_path = 'C:/Users/Logan Hallee/Desktop/1_26_result/result/'
-save_path = './result/'
-img_paths = natsorted(glob(file_path + '*.png'))
 
-r = 0.2
-h, w = np.array(cv2.imread(img_paths[0], 2)).shape
-h, w = int(h * r), int(w / 2 * r)
-preds = np.concatenate([np.array(cv2.resize(cv2.imread(img_paths[i], 2)[:, :5632], (h, w))).reshape(h, w, 1)
-                        for i in tqdm(range(len(img_paths)))], axis=2)
-# plt.imshow(preds[:,:,0])
-# plt.show()
+class interp_3d(object):
+	def __init__(self, config):
+		#paths
+		self.save_path = config.save_path
+		self.rotate = config.rotate
+		self.img_paths = natsorted(glob(config.img_path + '*.png')[28:30])
+		self.s = config.scale
+		self.d = config.density
+		self.type = config.type
 
-X, Y, Z = preds.shape
+	def run_interp(self):
+		h, w = np.array(cv2.imread(self.img_paths[0], 2)).shape
+		h, w = int(h * self.s), int(w / 2 * self.s)
+		if self.type == 'hev':
+			preds = np.concatenate(
+				[np.array(cv2.resize(cv2.imread(self.img_paths[i], 2)[:, :5632], (h, w))).reshape(h, w, 1)
+				 for i in tqdm(range(len(self.img_paths)))], axis=2)
+		elif self.type =='lob':
+			preds = np.concatenate(
+				[np.array(cv2.resize(cv2.imread(self.img_paths[i], 2)[:, 5632:], (h, w))).reshape(h, w, 1)
+				 for i in tqdm(range(len(self.img_paths)))], axis=2)
+		X, Y, Z = preds.shape
+		z_scale = 16 * self.s
+		grid_x = np.arange(0, X, 1)
+		grid_y = np.arange(0, Y, 1)
+		grid_z = np.arange(0, Z, 1) * z_scale
+		grid = (grid_x, grid_y, grid_z)
+		print('Sparse grid done')
+		interpolation = scipy.interpolate.RegularGridInterpolator(grid, preds)
+		print('Sparse Interpolation done')
+		sZ = (Z - 1) * z_scale
+		zF = int(Z / self.d)
+		print(X, Y, sZ, zF)
+		interp_arr = np.mgrid[0:X - 1:self.d, 0:Y - 1:self.d, 0:sZ:(sZ/zF)].\
+			reshape(3, int(((X - 1) / self.d) * ((Y - 1) / self.d) * zF)).T
+		interp_grid = interp_arr.tolist()
+		print('Dense grid done')
+		interp_pred = np.array(interpolation(interp_grid))
+		print('Dense Interpolation done')
+		points = np.array(interp_grid)
+		print(points.shape)
+		zero_colors = np.zeros((len(interp_pred), 2))
+		colors = np.hstack((interp_pred.reshape(len(interp_pred), 1), zero_colors)) / 255
+		deletes = []
+		for i in tqdm(range(len(colors)), desc='Removing Background'):
+			if colors[i][0] < 0.5:
+				deletes.append(i)
 
-s = 16 * r
-grid_x = np.arange(0, X, 1)
-grid_y = np.arange(0, Y, 1)
-grid_z = np.arange(0, Z, 1) * s
-grid = (grid_x, grid_y, grid_z)
+		points = np.delete(points, deletes, axis=0)
+		colors = np.delete(colors, deletes, axis=0)
+		pcd = open3d.geometry.PointCloud()
+		pcd.points = open3d.utility.Vector3dVector(points)
+		pcd.colors = open3d.utility.Vector3dVector(colors)
+		if self.rotate:
+			open3d.visualization.draw_geometries_with_animation_callback([pcd], self.rotate_view)
+		else:
+			open3d.visualization.draw_geometries([pcd])
 
-print('Sparse grid done')
-interpolation = scipy.interpolate.RegularGridInterpolator(grid, preds)
-print('Sparse Interpolation done')
-c = 1
-sZ = (Z - 1) * s
-sc = int(Z / c)
-# assert sZ % c == 0, 'Voxel scales and z stack scale must be divisible for mgrid to work'
-print(X, Y, sZ, sc)
-interp_arr = np.mgrid[0:X - 1:c, 0:Y - 1:c, 0:sZ:(sZ / sc)].reshape(3, int(((X - 1) / c) * ((Y - 1) / c) * sc)).T
-interp_grid = interp_arr.tolist()
-print('Dense grid done')
-V = np.array(interpolation(interp_grid))
-print('Dense Interpolation done')
-points = np.array(interp_grid)
-print(points.shape)
-zero_colors = np.zeros((len(V), 2))
-colors = np.hstack((V.reshape(len(V), 1), zero_colors)) / 255
+	def plot4d(self, data, X, Y, z_scaled):
+		# V3D = V.reshape(int((X-1)/c), int((Y-1)/c), int(sZ/c))
+		data = data.reshape(int((X-1)/self.d), int((Y-1)/self.d), int(z_scaled/self.d))
+		print('Plotting')
+		fig = plt.figure(figsize=(5, 5))
+		ax = fig.add_subplot(projection="3d")
+		ax.xaxis.pane.fill = False
+		ax.yaxis.pane.fill = False
+		ax.zaxis.pane.fill = False
+		mask = data > 0.9
+		idx = np.arange(int(np.prod(data.shape)))
+		x, y, z = np.unravel_index(idx, data.shape)
+		ax.scatter(x, y, z, c=data.flatten(), s=10.0 * mask, edgecolor='face', alpha=0.01, marker='o', cmap='Reds',
+		           linewidth=0)
+		plt.tight_layout()
+		plt.savefig("test_scatter_4d.png", dpi=250)
+		print('Saved')
 
-deletes = []
-for i in tqdm(range(len(colors)), desc='Removing Background'):
-	if colors[i][0] < 0.5:
-		deletes.append(i)
-
-points = np.delete(points, deletes, axis=0)
-colors = np.delete(colors, deletes, axis=0)
-# V3D = V.reshape(int((X-1)/c), int((Y-1)/c), int(sZ/c))
-
-pcd = open3d.geometry.PointCloud()
-pcd.points = open3d.utility.Vector3dVector(points)
-pcd.colors = open3d.utility.Vector3dVector(colors)
+	def rotate_view(self, vis):
+		ctr = vis.get_view_control()
+		ctr.rotate(10.0, 0.0)
+		return False
 
 
-# mesh = open3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(pcd, 0.03)
-# mesh.compute_vertex_normals()
-# open3d.visualization.draw_geometries([mesh], mesh_show_back_face=True)
-open3d.visualization.draw_geometries([pcd])
-def rotate_view(vis):
-	ctr = vis.get_view_control()
-	ctr.rotate(10.0, 0.0)
-	return False
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	# Model hyper-parameters
+	parser.add_argument('--img_path', type=str, default='C:/Users/Logan Hallee/Desktop/1_26_result/result/')
+	parser.add_argument('--save_path', type=str, default='./result/')
+	parser.add_argument('--scale', type=float, default=0.01)
+	parser.add_argument('--density', type=float, default=0.1)
+	parser.add_argument('--rotate', type=bool, default=False)
+	parser.add_argument('--type', type=str, default='hev')
+	config = parser.parse_args()
+	main = interp_3d(config)
+	main.run_interp()
 
-#open3d.visualization.draw_geometries_with_animation_callback([pcd], rotate_view)
 
 
-def plot4d(data):
-	print('Plotting')
-	fig = plt.figure(figsize=(5, 5))
-	ax = fig.add_subplot(projection="3d")
-	ax.xaxis.pane.fill = False
-	ax.yaxis.pane.fill = False
-	ax.zaxis.pane.fill = False
-	mask = data > 0.9
-	idx = np.arange(int(np.prod(data.shape)))
-	x, y, z = np.unravel_index(idx, data.shape)
-	ax.scatter(x, y, z, c=data.flatten(), s=10.0 * mask, edgecolor='face', alpha=0.01, marker='o', cmap='Reds',
-	           linewidth=0)
-	plt.tight_layout()
-	plt.savefig("test_scatter_4d.png", dpi=250)
-	print('Saved')
-# plt.show()
-
-# plot4d(V3D)
