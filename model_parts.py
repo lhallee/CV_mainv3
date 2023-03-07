d0b794f9ee78f3a62d8d4ca30390b467e05c615c
Logan Hallee <lhallee99@gmail.com>
2023-03-07 16:42:28 -0500

    loss updated

--- model_parts.py
+++ model_parts.py
@@ -39,7 +39,7 @@ class DiceBCELoss(nn.Module):
 
         intersection = (inputs * targets).sum()
         dice_loss = 1 - (2. * intersection + smooth) / (inputs.sum() + targets.sum() + smooth)
-        BCE = F.binary_cross_entropy(inputs, targets, reduction='mean')
+        BCE = F.binary_cross_entropy_with_logits(inputs, targets, reduction='mean')
         Dice_BCE = BCE + dice_loss
 
         return Dice_BCE
